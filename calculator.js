ea9d3437691bbc30bc58119313fe1fa5470d09b1
Butindi Bakisi Daniella <daniella.butindi-bakisi.etu@univ-lille.fr>
2026-08-03 21:46:26 +0200

    Evenement sur le bouton clear

--- calculator.js
+++ calculator.js
@@ -6,9 +6,16 @@ const displayNumber = function(){
     screen.value += this.textContent;
 }
 
+const clearScreen = function(){
+    const screen = document.getElementById("typingzone");
+    screen.value ="";
+}
+
 const setupListeners = function(){
+    const boutonClear = document.getElementById("clear");
     const boutonsNombre = document.querySelectorAll("#numbers-buttons button");
     const boutonsOperations = document.querySelectorAll("#operations-buttons button");
+    boutonClear.addEventListener("click", clearScreen);
     for (let i = 0; i < boutonsNombre.length; i++ ){
         boutonsNombre[i].addEventListener("click", displayNumber);
     }
